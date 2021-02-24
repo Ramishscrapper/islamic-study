35db696bcd68cd72da7e6915782c850c7ae6cf94
Ramish Hassan Riaz <hassanramish44@gmail.com>
2021-02-24 12:18:34 +0500

    Add A New Wallpaper Page

---
+++ assets/js/banner.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        items:1,
+        loop:true,
+        nav:true,
+        dots:true,
+        autoplay:true,
+        autoplaySpeed:1000,
+        smartSpeed:1500,
+        // autoplayHoverPause:true
+    });
+});
