04d259af2626f81555d549c330b5960d2d2ee172
Ramish Hassan Riaz <hassanramish44@gmail.com>
2021-03-07 12:36:22 +0500

    Adding Surah

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,3 +17,20 @@ AOS.init({
           }
         });
       }
+
+      function openTab(cityName,element,color) {
+        var i, tabcontent, tablinks;
+        tabcontent = document.getElementsByClassName("tabContent");
+        for (i = 0; i < tabcontent.length; i++) {
+          tabcontent[i].style.display = "none";
+        }
+        tablinks = document.getElementsByClassName("tablink");
+        for (i = 0; i < tablinks.length; i++) {
+          tablinks[i].style.backgroundColor = "";
+        }
+        document.getElementById(cityName).style.display = "block";
+        element.style.backgroundColor = color;
+      
+      }
+      // Get the element with id="defaultOpen" and click on it
+      document.getElementById("defaultOpen").click();
