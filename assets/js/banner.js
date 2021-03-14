f73e995cf764cb7132827dac319a1086fc19834f
Ramish Hassan <hassanramish44@gmail.com>
2021-03-14 10:21:01 -0700

    Removing Banner

--- assets/js/banner.js
+++ assets/js/banner.js
@@ -1,12 +1,27 @@
-$(document).ready(function () {
-    $(".owl-carousel").owlCarousel({
-        items:1,
-        loop:true,
-        nav:true,
-        dots:true,
-        autoplay:true,
-        autoplaySpeed:1000,
-        smartSpeed:1500,
-        autoplayHoverPause:true
-    });
-});
+var sickPrimary = {
+  autoplay: true,
+  autoplaySpeed: 2400,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  speed: 1800,
+  cssEase: "cubic-bezier(.84, 0, .08, .99)",
+  asNavFor: ".text-slider",
+  centerMode: true,
+  prevArrow: $(".prev"),
+  nextArrow: $(".next"),
+};
+
+var sickSecondary = {
+  autoplay: true,
+  autoplaySpeed: 2400,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  speed: 1800,
+  cssEase: "cubic-bezier(.84, 0, .08, .99)",
+  asNavFor: ".image-slider",
+  prevArrow: $(".prev"),
+  nextArrow: $(".next"),
+};
+
+$(".image-slider").slick(sickPrimary);
+$(".text-slider").slick(sickSecondary);
