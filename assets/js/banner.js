efb17f2938ea993b30faee233174fcd7ac7513ed
Ramishscrapper <hassanramish44@gmail.com>
2021-05-09 14:52:36 +0500

    Adding wallpaper to wallpaper page

--- assets/js/banner.js
+++ assets/js/banner.js
@@ -7,13 +7,15 @@ $(".owl-carousel").on("initialized.owl.carousel", () => {
 
 const $owlCarousel = $(".owl-carousel").owlCarousel({
   items: 1,
-  auto: true,
-  loop: true,
   nav: true,
   navText: [
     '<svg width="50" height="50" viewBox="0 0 24 24"><path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z"/></svg>',
-    '<svg width="50" height="50" viewBox="0 0 24 24"><path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z"/></svg>' /* icons from https://iconmonstr.com */,
+    '<svg width="50" height="50" viewBox="0 0 24 24"><path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z"/></svg>',
   ],
+  loop: true,
+  autoplay: true,
+  autoplayTimeout: 3000,
+  autoplayHoverPause: true,
 });
 
 $owlCarousel.on("changed.owl.carousel", (e) => {
