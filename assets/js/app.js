8f2a5b16fba30e0c89b6b485ed0ca0de6e95b6a9
Ramishscrapper <hassanramish44@gmail.com>
2021-05-11 14:20:47 +0500

    fix the issue of app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,36 +1,33 @@
 AOS.init({
-        offset: 350,
-        delay: 0, 
-        duration: 1000 
-} );
-      var acc = document.getElementsByClassName("surah");
-      var i;
-      
-      for (i = 0; i < acc.length; i++) {
-        acc[i].addEventListener("click", function() {
-          this.classList.toggle("active");
-          var panel = this.nextElementSibling;
-          if (panel.style.display === "block") {
-            panel.style.display = "none";
-          } else {
-            panel.style.display = "block";
-          }
-        });
-      }
+  offset: 350,
+  delay: 0,
+  duration: 1000,
+});
+var acc = document.getElementsByClassName("surah");
+var i;
 
-      function openTab(cityName,element,color) {
-        var i, tabcontent, tablinks;
-        tabcontent = document.getElementsByClassName("tabContent");
-        for (i = 0; i < tabcontent.length; i++) {
-          tabcontent[i].style.display = "none";
-        }
-        tablinks = document.getElementsByClassName("tablink");
-        for (i = 0; i < tablinks.length; i++) {
-          tablinks[i].style.backgroundColor = "";
-        }
-        document.getElementById(cityName).style.display = "block";
-        element.style.backgroundColor = color;
-      
-      }
-      // Get the element with id="defaultOpen" and click on it
-      document.getElementById("defaultOpen").click();
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+function openTab(cityName, element, color) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabContent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+  }
+  document.getElementById(cityName).style.display = "block";
+  element.style.backgroundColor = color;
+}
