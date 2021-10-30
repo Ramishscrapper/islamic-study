8422631eae8f148a8dbe9684be8b3fb3ce5cc079
Ramishscrapper <hassanramish44@gmail.com>
2021-10-30 22:39:44 +0500

    Converting the Quran Surah adding using Template Literals

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,31 +3,13 @@ AOS.init({
   delay: 0,
   duration: 1000,
 });
-var acc = document.getElementsByClassName("surah");
-var i;
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function () {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
+// Quran Surah Tabs
+$(document).ready(function () {
+  var surahName = $(".surahName");
+  $(surahName).next(".panel").hide();
+  $(surahName).on("click", function () {
+    $(this).next(".panel").stop(500).slideToggle();
+    $(this).toggleClass("active");
   });
-}
-
-function openTab(cityName, element, color) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabContent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablink");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].style.backgroundColor = "";
-  }
-  document.getElementById(cityName).style.display = "block";
-  element.style.backgroundColor = color;
-}
+});
---
+++ assets/js/quran-surah-template-literal.js
@@ -0,0 +1,271 @@
+var quranSurahData = [
+  // Surah Number One (1)
+  {
+    name: "1: Surat-ul-Fateha",
+    verses: 7,
+    iframe: `
+    <iframe
+              src="https://www.youtube.com/embed/eryPs2RfpTU"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+    `,
+  },
+
+  // Surah Number Two (2)
+  {
+    name: "2: Surat-ul-Baqara",
+    verses: 286,
+    rukus: 40,
+    iframe: `
+    <iframe
+              src="https://www.youtube.com/embed/ZjZneawVueY"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+    `,
+  },
+
+  // Surah Number Three (3)
+  {
+    name: "3: Surat Aal-e-Imran",
+    verses: 200,
+    rukus: 20,
+    iframe: `
+    <iframe
+              src="https://www.youtube.com/embed/09_saEjBpYU"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+    `,
+  },
+
+  // Surah Number Four (4)
+  {
+    name: "4: Surat-un-Nissa",
+    verses: 176,
+    rukus: 24,
+    iframe: `
+    <iframe
+              src="https://www.youtube.com/embed/fMo163Ya3SY"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+    `,
+  },
+
+  // Surah Number Five (5)
+  {
+    name: "5: Surat-ul-Maeeda",
+    verses: 120,
+    rukus: 16,
+    iframe: `
+    <iframe
+              src="https://www.youtube.com/embed/9zqVkeoAP7U"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+    `,
+  },
+
+  // Surah Number Six (6)
+  {
+    name: "6: Surat-ul-Anaam",
+    verses: 165,
+    rukus: 20,
+    iframe: `
+   <iframe
+              src="https://www.youtube.com/embed/liK3RH8f8QA"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+    `,
+  },
+
+  // Surah Number Seven (7)
+  {
+    name: "7: Surat Al-A'raf",
+    verses: 206,
+    rukus: 24,
+    iframe: `
+            <iframe
+              src="https://www.youtube.com/embed/iYcFwRlSUms"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>      
+    `,
+  },
+
+  // Surah Number Eight (8)
+  {
+    name: "8: Surat Al-Anfal",
+    verses: 75,
+    rukus: 10,
+    iframe: `
+            <iframe
+              src="https://www.youtube.com/embed/3fDDtUnvta8"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>    
+    `,
+  },
+
+  // Surah Number Nine (9)
+  {
+    name: "9: Surat At-Tawbah",
+    verses: 129,
+    rukus: 16,
+    iframe: `
+            <iframe
+              src="https://www.youtube.com/embed/o8oghQgLJgY"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe> 
+    `,
+  },
+
+  // Surah Number Ten (10)
+  {
+    name: "10: Surat Yunus",
+    verses: 109,
+    rukus: 11,
+    iframe: `
+            <iframe
+              src="https://www.youtube.com/embed/We3rtNjBm7Q"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+    `,
+  },
+
+  // Surah Number Eleven (11)
+  {
+    name: "11: Surat Hud",
+    verses: 123,
+    rukus: 10,
+    iframe: `
+            <iframe
+              src="https://www.youtube.com/embed/OAnFuUtaC2k"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+    `,
+  },
+
+  // Surah Number Tweleve (12)
+  {
+    name: "12: Surat Yusuf",
+    verses: 111,
+    iframe: `
+            <iframe
+              src="https://www.youtube.com/embed/iR611FNDTKM"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+    `,
+  },
+
+  // Surah Number Thirteen (13)
+  {
+    name: "13: Surat Ar-Ra'd",
+    verses: 43,
+    rukus: 6,
+    iframe: `
+            <iframe
+              src="https://www.youtube.com/embed/lQDY7mSAk8M"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+    `,
+  },
+
+  // Surah Number Fourteen (14)
+  {
+    name: "14: Surat Ibrahim",
+    verses: 52,
+    rukus: 7,
+    iframe: `
+           <iframe
+              src="https://www.youtube.com/embed/g6Eb5HmxGLY"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+    `,
+  },
+
+  // Surah Number Fivteen (15)
+  {
+    name: "15: Surat Al-Hijr",
+    verses: 99,
+    rukus: 6,
+    iframe: `
+           <iframe
+              src="https://www.youtube.com/embed/GEEWx8bGPXE"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+    `,
+  },
+
+  // Surah Number Sixteen (16)
+  {
+    name: "16: Surat An-Nahl",
+    verses: 128,
+    iframe: `
+         <iframe
+              src="https://www.youtube.com/embed/JmjP3wUqg74"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+    `,
+  },
+];
+
+var quranSurahTemplateStart = `
+<h2 class="quran-surah-title">Quran Surah</h2>
+`;
+
+// Quran Surah Tab HTML Template
+function quranSurahTemplate(surah) {
+  return `
+      <div class="quran-surah-with-tab">
+          <button class="surah surahName">${surah.name}</button>
+          <div class="panel">
+            <ul>
+              <li><span class="surah-details">No. of verses</span>${surah.verses}</li>
+              <li><span class="surah-details">No. of rukus</span> ${surah.rukus}</li>
+            </ul>
+            ${surah.iframe}
+          </div>
+        </div>
+  `;
+}
+
+document.getElementById("quran-surah").innerHTML = `
+  ${quranSurahTemplateStart}
+  ${quranSurahData.map(quranSurahTemplate).join("")}
+`;
