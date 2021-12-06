e0403b02ff669e2209294141e0ba3cfd1890ae05
Ramishscrapper <hassanramish44@gmail.com>
2021-12-06 23:58:57 +0500

    Change Email

--- assets/js/mail.js
+++ assets/js/mail.js
@@ -18,7 +18,7 @@ function sendmail() {
 
   Email.send({
     SecureToken: "fbf31702-bb7f-4a4e-9c1c-4ccf17ee777f",
-    To: "hassanramish44@gmail.com",
+    To: "ramishscrapper@gmail.com",
     From: "ramishscrapper@gmail.com",
     Subject: "New message on contact from " + name,
     Body: Body,
