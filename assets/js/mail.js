7a5522818060870367a27e061c59689a15b2b592
Ramishscrapper <hassanramish44@gmail.com>
2021-12-08 00:24:58 +0500

    Improve the Email Javascript Code

--- assets/js/mail.js
+++ assets/js/mail.js
@@ -3,24 +3,22 @@ function sendmail() {
   var email = $("#Sender").val();
   var subject = $("#Subject").val();
   var message = $("#Message").val();
-  // var body = $('#body').val();
 
-  var Body =
-    "Name: " +
-    name +
-    "<br>Email: " +
-    email +
-    "<br>Subject: " +
-    subject +
-    "<br>Message: " +
-    message;
-  //console.log(name, phone, email, message);
+  var Body = `
+            ${name}
+
+            ${email}
+
+            ${subject}
+
+            ${message}
+    `;
 
   Email.send({
     SecureToken: "fbf31702-bb7f-4a4e-9c1c-4ccf17ee777f",
     To: "ramishscrapper@gmail.com",
     From: `${email}`,
-    Subject: "New message on contact from " + name,
+    Subject: `New message on contact from: ${name}`,
     Body: Body,
   }).then((message) => {
     if (message == "OK") {
